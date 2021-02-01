8f17b9a35240d9e8b4ef6c86dd1c0895298df9ca
manazjija <manazmeu21@mail.ru>
2021-02-01 19:24:01 +0300

    edit comments

--- src/components/ProjectPage/ComentsBlock/CommentsList/Comment/Comment.jsx
+++ src/components/ProjectPage/ComentsBlock/CommentsList/Comment/Comment.jsx
@@ -1,19 +1,50 @@
 import React from 'react';
 import Styles from './comment.module.css'
 
-const Comment = (props) =>{
-    return (
-        <div>
-            {(props.user === props.author)?
-                <div className={Styles.me}>{props.author}:</div>
-                :
-                <div>{props.author}:</div>
-            }
+class Comment extends React.Component{
+
+    state = {
+        editMode: false
+    };
+
+    activateEditMode = () => {
+        this.setState({
+            editMode: true
+        })
+    };
+    deactivateEditMode = () => {
+        this.setState({
+            editMode: false
+        })
+    };
+
+    render(){
+        return (
             <div>
-                {props.text}
+                {(this.props.user === this.props.author) ?
+                    <div>
+                        <div className={Styles.me}>{this.props.author}:</div>
+                        {!this.state.editMode &&
+                        <div onDoubleClick={this.activateEditMode}>
+                            {this.props.text}
+                        </div>
+                        }
+                        {this.state.editMode &&
+                        <div>
+                            <textarea autoFocus={true} onBlur={this.deactivateEditMode} name="" id="" cols="50" rows="4"
+                                      value={this.props.text}/>
+                        </div>
+                        }
+                    </div>
+                    :
+                    <div>
+                        <div>{this.props.author}:</div>
+                        <div>{this.props.text}:</div>
+                    </div>
+                }
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default Comment
